27043322e17818ca7f09249a051228b33d7163b0
bhkhanna <kbharat1307@gmail.com>
2020-05-10 16:13:59 +0530

    App Completed
    Signed-off-by: bhkhanna <kbharat1307@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,76 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 10),
-            padding: EdgeInsets.all(10),
-            child: Text('Hello!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/bharat_img.jpg'),
+                radius: 50,
+              ),
+              Text(
+                'Bharat Khanna',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'CO-FOUNDER',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      '+91 965 023 4691',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal[900],
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  )),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'kbharat1307@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                  ))
+            ],
           ),
         ),
       ),
